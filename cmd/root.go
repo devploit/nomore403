4e607bd7a5584d833da02101daf08d738c5413b7
clarabennett2626 <clarabennett2626@gmail.com>
2026-02-15 14:55:24 -0500

    feat: add --version flag
    Add --version flag support using cobra's built-in version functionality.
    Version and build date are injected via ldflags at build time (already
    configured in .goreleaser.yaml), with sensible defaults for dev builds.
    
    Closes #52

--- cmd/root.go
+++ cmd/root.go
@@ -81,6 +81,12 @@ var rootCmd = &cobra.Command{
 	},
 }
 
+// SetVersionInfo sets the version information for the root command.
+func SetVersionInfo(version, buildDate string) {
+	rootCmd.Version = version
+	rootCmd.SetVersionTemplate(fmt.Sprintf("nomore403 version %s (built %s)\n", version, buildDate))
+}
+
 // Execute adds all child commands to the root command and sets flags appropriately.
 // This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
